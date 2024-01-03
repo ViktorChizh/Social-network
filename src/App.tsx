4d18fc97fdb0958b13963dbbfacd91cbb7de3e67
ViktorChizh <victor.chizh@mail.ru>
2024-01-03 17:20:27 +0300

    sprint 1-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
      <h1>Hello, samurai! Let's go!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
