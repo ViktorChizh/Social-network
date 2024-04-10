cd0097703f57bc2cdc2fd3806326b45c56423010
ViktorChizh <victor.chizh@mail.ru>
2024-04-10 10:13:15 +0300

    profile status
    Signed-off-by: ViktorChizh <victor.chizh@mail.ru>

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -4,18 +4,20 @@ import { Redirect } from "react-router-dom"
 import { compose } from "redux"
 import { StateReduxType } from "redux/_Store-Redux"
 
-class Login_ extends Component<MStPType> {
+class Login_ extends Component<mapStateToPropsType> {
 	render() {
 		if (this.props.isAuth) return <Redirect to={"/profile"} />
 		return <h1>LOGIN</h1>
 	}
 }
 
-const mapStateToProps = (state: StateReduxType): MStPType => ({
+const mapStateToProps = (state: StateReduxType): mapStateToPropsType => ({
 	isAuth: state.auth.isAuth,
 })
-type MStPType = {
+type mapStateToPropsType = {
 	isAuth: boolean
 }
 
-export const Login = compose<ComponentType>(connect(mapStateToProps, {}))(Login_)
+export const Login = compose<ComponentType>(
+	connect<mapStateToPropsType, unknown, unknown, StateReduxType>(mapStateToProps, {}),
+)(Login_)
