c7c9ac33dbc6857518a28266e53dcd7c770dd41d
ViktorChizh <victor.chizh@mail.ru>
2024-07-08 08:12:13 +0300

    refactor
    Signed-off-by: ViktorChizh <victor.chizh@mail.ru>

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React from "react"
 import { Redirect } from "react-router-dom"
 import { addDialogAC, addMessageAC } from "../../redux/DialogReducer"
 import { TextareaWithButton, TextForm } from "../common/textareaWithButton/TextareaWithButton"
@@ -7,7 +7,7 @@ import s from "./Dialogs.module.css"
 import { DialogsContainerType } from "./DialogsContainer"
 import { Message } from "./message/Message"
 
-export const Dialogs: FC<DialogsContainerType> = ({ dialog, dispatch, isLoggedIn }) => {
+export const Dialogs = ({ dialog, dispatch, isLoggedIn }:DialogsContainerType) => {
 	document.title = "SocialNetwork - dialogs"
 	const addDialog = (formData: TextForm) => dispatch(addDialogAC(formData.addDialog))
 	const addMessage = (formData: TextForm) => dispatch(addMessageAC(formData.addMessage))
@@ -17,16 +17,17 @@ export const Dialogs: FC<DialogsContainerType> = ({ dialog, dispatch, isLoggedIn
 		<div className={s.dialogs}>
 			<div className={s.dialogsItem}>
 				<span>Dialogs</span>
-				{dialog.dialogs.map((i) => (
-					<DialogItem key={i.id} id={i.id} name={i.name} />
+				{dialog.dialogs.map((d) => (
+					<DialogItem key={d.id} id={d.id} name={d.name} />
 				))}
 				<TextareaWithButton buttonName="ADD DIALOG" onSubmit={addDialog} placeholder="addDialog" name="addDialog" />
 			</div>
 			<div className={s.messages}>
-				{dialog.messages.map((i) => (
-					<Message key={i.id} id={i.id} message={i.message} />
+				<span>Messages</span>
+				{dialog.messages.map((m) => (
+					<Message key={m.id} id={m.id} message={m.message}/>
 				))}
-				<TextareaWithButton buttonName="ADD MESSAGE" onSubmit={addMessage} placeholder="addMessage" name="addMessage" />
+				<TextareaWithButton buttonName="ADD MESSAGE" onSubmit={addMessage} placeholder="addMessage" name="addMessage"/>
 			</div>
 		</div>
 	)
