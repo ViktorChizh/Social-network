5c9853b9877a48fb5d231a043b8d83548bf83779
ViktorChizh <victor.chizh@mail.ru>
2024-07-12 16:48:21 +0300

    refactor
    Signed-off-by: ViktorChizh <victor.chizh@mail.ru>

--- src/components/dialogs/message/Message.tsx
+++ src/components/dialogs/message/Message.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from "react"
-import s from "../Dialogs.module.css"
+import React from 'react'
+import s from '../Dialogs.module.css'
 
 export type MessageType = {
 	id: number
 	message: string
 }
 
-export const Message: FC<MessageType> = ({ id, message }) => {
+export const Message = ({ id, message }: MessageType) => {
 	return (
 		<div className={s.message} key={id}>
 			{message}
