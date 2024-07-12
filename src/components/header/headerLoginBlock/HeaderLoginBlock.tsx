51bd3cadba90dbbf510dfb2374890c259bf9f4bb
ViktorChizh <victor.chizh@mail.ru>
2024-07-12 16:52:09 +0300

    refactor
    Signed-off-by: ViktorChizh <victor.chizh@mail.ru>

--- src/components/header/headerLoginBlock/HeaderLoginBlock.tsx
+++ src/components/header/headerLoginBlock/HeaderLoginBlock.tsx
@@ -1,22 +1,22 @@
-import s from "components/header/headerLoginBlock/HeaderLoginBlock.module.css"
-import React, { FC, memo } from "react"
+import s from 'components/header/headerLoginBlock/HeaderLoginBlock.module.css'
+import React, {memo} from 'react'
 
-type PropsType = {
+type Props = {
 	isLoggedIn: boolean
 	login: string | null
 	ownUserAvatar: string
 	onClickHandler: () => void
 }
 
-export const HeaderLoginBlock: FC<PropsType> = memo((props) => {
+export const HeaderLoginBlock = memo(({isLoggedIn, login, ownUserAvatar, onClickHandler}:Props) => {
 	return (
 		<div className={s.loginBlock}>
-			{props.isLoggedIn && (
+			{isLoggedIn && (
 				<div className={s.login}>
-					<img src={props.ownUserAvatar} alt="" className={s.loginImg} />
+					<img src={ownUserAvatar} alt="" className={s.loginImg} />
 					<div>
-						<p>{props.login}</p>
-						<p onClick={props.onClickHandler} className={s.logout}>
+						<p>{login}</p>
+						<p onClick={onClickHandler} className={s.logout}>
 							logout
 						</p>
 					</div>
