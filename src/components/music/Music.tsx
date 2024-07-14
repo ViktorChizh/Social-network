9233d3f0664603674b3d14d2e3849a670cbb5249
ViktorChizh <victor.chizh@mail.ru>
2024-07-14 10:26:14 +0300

    refactor
    Signed-off-by: ViktorChizh <victor.chizh@mail.ru>

--- src/components/music/Music.tsx
+++ src/components/music/Music.tsx
@@ -1,41 +1,36 @@
 import React from "react"
 import s from "./Music.module.css"
 
-type MusicBaseType = {
+type MusicBase = {
 	radioTitle: string
 	radioLink: string
 	imgLink: string
-	imgAlt: string
 }
 
-const musicBase: MusicBaseType[] = [
+const musicBase: MusicBase[] = [
 	{
 		radioTitle: "Радио Relax FM",
 		radioLink: "https://101.ru/radio/channel/24",
 		imgLink:
 			"https://cdn2.101.ru/proxy/vardata/modules/channel/image/8176b78a2460ef4f63aa1907a3b46099.jpg?w=300&h=300&pos=center&t=1669111592",
-		imgAlt: "Relax FM",
 	},
 	{
 		radioTitle: "Радио Deep House",
 		radioLink: "https://101.ru/radio/channel/173",
 		imgLink:
 			"https://cdn0.101.ru/proxy/vardata/modules/channel/image/18e59e661597a41b54918781ff5f292e.png?w=300&h=300&pos=center&t=1574857944",
-		imgAlt: "Deep House",
 	},
 	{
 		radioTitle: "Радио Enigma",
 		radioLink: "https://101.ru/radio/channel/175",
 		imgLink:
 			"https://cdn2.101.ru/proxy/vardata/modules/channel/image/8700bf0eb42cee25219de44034753570.jpg?w=300&h=300&pos=center&t=1669797633",
-		imgAlt: "Enigma",
 	},
 	{
 		radioTitle: "Радио Depeche Mode",
 		radioLink: "https://101.ru/radio/channel/105",
 		imgLink:
 			"https://cdn2.101.ru/proxy/vardata/modules/channel/image/7eeb2f4860de4a03bdcf6dc195088df2.png?w=300&h=300&pos=center&t=1565608474",
-		imgAlt: "Depeche Mode",
 	},
 ]
 
@@ -46,7 +41,7 @@ const Music = () => {
 			{musicBase.map((e) => {
 				return (
 					<a className={s.block} href={e.radioLink} target="_blanc" key={e.radioTitle}>
-						<img src={e.imgLink} alt={e.imgAlt} />
+						<img src={e.imgLink} alt={e.radioTitle} />
 						{e.radioTitle}
 					</a>
 				)
