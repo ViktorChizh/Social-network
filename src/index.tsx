d3b06dea2bd8b345ca86712a46bccb37b62c402a
ViktorChizh <victor.chizh@mail.ru>
2024-07-19 17:40:56 +0300

    refactor
    Signed-off-by: ViktorChizh <victor.chizh@mail.ru>

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from "app/App"
 import { store } from "./redux/_Store-Redux"
 import { Provider } from "react-redux"
 import { HashRouter } from "react-router-dom"
-// GitHubPages uncorrect working with BrouserRouter
+// GitHubPages uncorrect working with BrowserRouter
 ReactDOM.render(
 	<HashRouter>
 		<Provider store={store}>
